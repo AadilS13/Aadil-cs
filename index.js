1c86d75c41e9654ec247e497f4e10e926503fffc
unknown <aadilsardar14@gmail.com>
2025-02-24 20:38:53 +0000

    adilsa

--- index.js
+++ index.js
@@ -5,5 +5,36 @@
  * IF nuber is a multiple of 5 you're going to console.log('bar')
  * IF number is a multiple of 3 and 5 you're going to console.log('foobar')
  * At the end of the 100 times, conosle.log('im finished')
- */
+*/
 
+/*
+
+for( loop 100 times ) {
+
+    if number is multiple of 3 {
+        log foo
+    }
+    
+    if number if multiple of 5 {
+        log bar
+    }
+
+
+    if number if multiple of 3 and 5 {
+        log foobar
+    }
+
+}
+
+log finished
+
+*/
+
+for(var number = 0; number < 100; number++) {
+    // Do code here
+}
+
+
+if( number % 3 == 0) {
+    console.log('foo')
+}
