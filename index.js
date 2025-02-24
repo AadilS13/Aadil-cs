4493ed1be9957f8f747489ebc5e8929a1c68756b
unknown <aadilsardar14@gmail.com>
2025-02-24 20:26:32 +0000

    starter for JS

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Application, that loops 100 times
+ * 
+ * IF number is a multiple 3 you're going to console.log('foo')
+ * IF nuber is a multiple of 5 you're going to console.log('bar')
+ * IF number is a multiple of 3 and 5 you're going to console.log('foobar')
+ * At the end of the 100 times, conosle.log('im finished')
+ */
+
